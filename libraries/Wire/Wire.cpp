2b9e30cac9e237d518061ffe6d8f09cccb8c3869
giulcioffi <g.cioffi@arduino.cc>
2021-01-13 18:30:50 +0100

    Change thread stack sizes

--- libraries/Wire/Wire.cpp
+++ libraries/Wire/Wire.cpp
@@ -23,9 +23,7 @@
 #include "Wire.h"
 #include "pinDefinitions.h"
 
-arduino::MbedI2C::MbedI2C(int sda, int scl) : _sda(digitalPinToPinName(sda)), _scl(digitalPinToPinName(scl)), usedTxBuffer(0) {}
-
-arduino::MbedI2C::MbedI2C(PinName sda, PinName scl) : _sda(sda), _scl(scl), usedTxBuffer(0) {}
+arduino::MbedI2C::MbedI2C(int sda, int scl) : _sda(sda), _scl(scl), usedTxBuffer(0), slave_th(osPriorityNormal, 2048) {}
 
 void arduino::MbedI2C::begin() {
 	master = new mbed::I2C(_sda, _scl);
