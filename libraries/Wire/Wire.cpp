dfacc5f31e23a725774d56f5d9f5e4382846e06a
giulcioffi <g.cioffi@arduino.cc>
2021-05-03 16:59:27 +0200

    Increment max buffer size

--- libraries/Wire/Wire.cpp
+++ libraries/Wire/Wire.cpp
@@ -140,7 +140,7 @@ void arduino::MbedI2C::receiveThd() {
 			case mbed::I2CSlave::WriteGeneral:
 			case mbed::I2CSlave::WriteAddressed:
 				rxBuffer.clear();
-				char buf[72];
+				char buf[264];
 				c = slave->read(buf, sizeof(buf));
 				for (buf_idx = 0; buf_idx < c; buf_idx++) {
 					if (rxBuffer.availableForStore()) {
